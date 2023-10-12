77488776d14846a76a05ef46c9a2609e3e595d1d
Karim Elmaaroufi <elmaaroufi@berkeley.edu>
2023-10-12 16:33:41 -0700

    More skeleton code and add openai dep

--- src/adapters/openai_adapter.py
+++ src/adapters/openai_adapter.py
@@ -1,18 +1,13 @@
 import openai
+import os
 
 
 class OpenAIAdapter:
     """
     This class servers as a wrapper for the OpenAI API.
-    The default strategy is to do zero-shot prompting whichover doesn't work well given
-    that GPT does not know about scenic.
-    Strategies that we need to implement:
-    - Few-shot learning
-    - Fine-tuning on the scenic codebase: https://platform.openai.com/docs/guides/fine-tuning
-    - Function calling: https://platform.openai.com/docs/guides/gpt/function-calling
     """
-    def __init__(self, api_key):
-        openai.api_key = api_key
+    def __init__(self):
+        openai.api_key = os.environ["OPENAI_API_KEY"]
 
     def get_response(self, prompt, **kwargs):
         return openai.Completion.create(prompt=prompt, **kwargs)
