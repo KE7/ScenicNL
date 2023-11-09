ad8f88624cf60bfd9c40d4ebdfe6cc20fb67ca53
Devan Shanker <devanshanker@berkeley.edu>
2023-11-09 15:57:32 -0800

    Automatic output directory generation and scenic compilation for large batch runs

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import click
 import os
 from pathlib import Path
+import scenic
 from src.adapters.model_adapter import ModelAdapter
 from src.adapters.openai_adapter import OpenAIAdapter, OpenAIModel
 from src.common import ModelInput, LLMPromptType
 import sys
-import click
 
 
 @click.group()
@@ -21,12 +22,32 @@ def main():
     ),
 )
 @click.option(
+    "--output-path",
+    type=click.Path(
+        file_okay=False,
+        dir_okay=True,
+    ),
+    default="outputs",
+    show_default=True,
+    help="Path to output directory for results.",
+)
+@click.option(
+    "--example-path",
+    type=click.Path(
+        file_okay=False,
+        dir_okay=True,
+    ),
+    default="examples",
+    show_default=True,
+    help="Path to examples for few-shot training.",
+)
+@click.option(
     "--cache-path",
     type=click.Path(
         file_okay=True,
         dir_okay=False,
     ),
-    default="/users/ke/Documents/Cal/research/scenicNL/cache.db",
+    default="cache.db",
     show_default=True,
     help="Path to SQLite3 database for caching.",
 )
@@ -39,6 +60,8 @@ def main():
 
 def main(
     query_set: Path,
+    output_path: Path,
+    example_path: Path,
     cache_path: Path,
     ignore_cache: bool
 ) -> None:
@@ -52,28 +75,45 @@ def main(
             print(line.strip())
 
     adapter = OpenAIAdapter(OpenAIModel.GPT_35_TURBO)
+    prompt_type = LLMPromptType.PREDICT_SCENIC_TUTORIAL
 
-    examples, examples_dir = [], 'examples'
+    example_list = []
 
-    for filename in os.listdir(examples_dir):
+    for filename in os.listdir(example_path):
         if filename.endswith('.txt'):
-            file_path = os.path.join(examples_dir, filename)
+            file_path = os.path.join(example_path, filename)
             if os.path.isfile(file_path):
                 with open(file_path, 'r') as file:
-                    examples.append(file.read())
+                    example_list.append(file.read())
 
     model_input_list = []
     for query in query_list:
-        model_input = ModelInput(examples=examples, nat_lang_scene_des=query)
+        model_input = ModelInput(examples=example_list, nat_lang_scene_des=query)
         model_input_list.append(model_input)
 
-    for output in adapter.predict_batch(
+    scenic_path = os.path.join(output_path, prompt_type.value)
+    if not os.path.exists(scenic_path):
+        os.makedirs(scenic_path)
+
+    compile_pass, compile_fail = 0, 0
+    for index, outputs in enumerate(adapter.predict_batch(
             model_inputs=model_input_list, 
             cache_path=cache_path, num_predictions=1, 
-            temperature=0, max_tokens=500, prompt_type=LLMPromptType.PREDICT_PYTHON_API):
-        print(output[0])
-        print('\n\n\n')
+            temperature=0, max_tokens=500, prompt_type=prompt_type)):
+        for attempt, output in enumerate(outputs):
+            print(f'{output}\n\n')
+            fname = os.path.join(scenic_path, f'{index}-{attempt}.scenic')
+            with open(fname, 'w') as f:
+                f.write(output)
+            try:
+                scenic.scenarioFromFile(fname, mode2D=True)
+                compile_pass += 1
+            except Exception as e:
+                print(f'Failed with error: {e}')
+                compile_fail += 1
+            print('----------------\n\n')
 
+    print(f'Compilation success rate: {round((100*compile_pass/(compile_fail+compile_pass)), 2)}%')
 
 def _launch():
     # to stop Click handling errors
