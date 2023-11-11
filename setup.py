5033598115d9e471693cc3b0689691bf104c8826
Devan Shanker <devanshanker@berkeley.edu>
2023-11-10 20:41:01 -0800

    Adding web scraper

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
+        "bs4",
         "scenic==3.0.0b2",
         "openai>=0.28,<=0.28.1",
         "pdf2image",
