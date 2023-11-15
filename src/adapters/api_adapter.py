cd932f0541a79127224e388f8cac72475e900d1d
Devan Shanker <devanshanker@berkeley.edu>
2023-11-14 20:18:26 -0800

    API adapter typing

--- src/adapters/api_adapter.py
+++ src/adapters/api_adapter.py
@@ -12,6 +12,7 @@ Not adding function types yet since the LLM output is not consistent and the try
 @TODO: Devan make the API usage 1. more closely resemble UCLID5 paper 2. find better indentation solution
 """
 
+from typing import Any
 
 class Scenic3:
 
@@ -19,49 +20,49 @@ class Scenic3:
         self.code = []
         self._indent = ' ' * 4
 
-    def set_map(self, map_name:any, indent=0):
+    def set_map(self, map_name:Any, indent:int=0):
         indent_str = self._indent * indent
         try:
             self.code.append(f"{indent_str}param map = localPath('{map_name}')")
         except:
             self.code.append(f"{str(indent_str)}param map = localPath('{map_name}')")
 
-    def set_model(self, model_name:any, indent=0):
+    def set_model(self, model_name:Any, indent:int=0):
         indent_str = self._indent * indent
         try: self.code.append(f"{indent_str}model {model_name}")
         except: self.code.append(f"{(indent_str)}model {str(model_name)}")
 
-    def define_constant(self, name:any, value:any, indent=0):
+    def define_constant(self, name:Any, value:Any, indent:int=0):
         indent_str = self._indent * indent
         try: self.code.append(f"{indent_str}{name} = {value}")
         except: self.code.append(f"{indent_str}{str(name)} = {str(value)}")
 
-    def define_behavior(self, name:any, indent=0, **kwargs):
+    def define_behavior(self, name:Any, indent:int=0, **kwargs):
         indent_str = self._indent * indent
         kwargs_str = ', '.join(f'{str(k)}={str(v)}' for k, v in kwargs.items())
         try: self.code.append(f"{indent_str}behavior {name}({kwargs_str}):")
         except: self.code.append(f"{indent_str}behavior {str(name)}({kwargs_str}):")
 
-    def do(self, behavior_name:any, indent=0, **kwargs):
+    def do(self, behavior_name:Any, indent:int=0, **kwargs):
         indent_str = self._indent * indent
         # behavior_name = str(behavior_name)
         kwargs_str = ', '.join(f'{str(k)}={str(v)}' for k, v in kwargs.items())
         try: self.code.append(f"{indent_str}do {behavior_name}({kwargs_str})")
         except: self.code.append(f"{indent_str}do {str(behavior_name)}({kwargs_str})")
 
-    def do_while(self, behavior_name:any, var_name:any, condition:any, indent=0):
+    def do_while(self, behavior_name:Any, var_name:Any, condition:Any, indent:int=0):
         indent_str = self._indent * indent
-        # behavior_name:any, var_name:any, condition = str(behavior_name), str(var_name), str(condition)
+        # behavior_name:Any, var_name:Any, condition = str(behavior_name), str(var_name), str(condition)
         try: self.code.append(f"{indent_str}do {behavior_name}({var_name}) while {condition}")
         except: self.code.append(f"{indent_str}do {str(behavior_name)}({str(var_name)}) while {condition}")
 
-    def do_until(self, behavior_name:any, var_name:any, condition:any, indent=0):
+    def do_until(self, behavior_name:Any, var_name:Any, condition:Any, indent:int=0):
         indent_str = self._indent * indent
-        # behavior_name:any, var_name = str(behavior_name), str(var_name)
+        # behavior_name:Any, var_name = str(behavior_name), str(var_name)
         try: self.code.append(f"{indent_str}do {behavior_name}({var_name}) until {condition}")
         except: self.code.append(f"{indent_str}do {str(behavior_name)}({var_name}) until {condition}")
 
-    def try_except(self, try_behavior, except_behavior, indent=0):
+    def try_except(self, try_behavior, except_behavior, indent:int=0):
         indent_str = self._indent * indent
         try_behavior, except_behavior = str(try_behavior), str(except_behavior)
         self.code.append(f"{indent_str}try:")
@@ -71,19 +72,19 @@ class Scenic3:
         try: self.code.append(f"{indent_str+self._indent}do {except_behavior}")
         except: self.code.append(f"{indent_str+self._indent}do {str(except_behavior)}")
 
-    def interrupt(self, condition:any, indent=0, *args):
+    def interrupt(self, condition:Any, indent:int=0, *args):
         indent_str = self._indent * indent
         args_str = ', '.join(str(arg) for arg in args)
         try: self.code.append(f"{indent_str}interrupt when {condition}({args_str}):")
         except: self.code.append(f"{indent_str}interrupt when {str(condition)}({args_str}):")
 
-    def take(self, action_name:any, indent=0, **params):
+    def take(self, action_name:Any, indent:int=0, **params):
         indent_str = self._indent * indent
         params_str = ', '.join(f'{str(k)}={str(v)}' for k, v in params.items())
         try: self.code.append(f"{indent_str}take {action_name}({params_str})")
         except: self.code.append(f"{indent_str}take {str(action_name)}({params_str})")
 
-    def new(self, var_name:any, obj_type:any, at=None, indent=0, **kwargs):
+    def new(self, var_name:Any, obj_type:Any, at=None, indent:int=0, **kwargs):
         indent_str = self._indent * indent
         try: new_line = f"{indent_str}{var_name} = new {obj_type.capitalize()}"
         except: new_line = f"{indent_str}{str(var_name)} = new {str(obj_type).capitalize()}"
@@ -96,7 +97,7 @@ class Scenic3:
 
         self.code.append(new_line)
 
-    def spatial_relation(self, obj1:any, keyword:any, obj2:any, distance=None, indent=0):
+    def spatial_relation(self, obj1:Any, keyword:Any, obj2:Any, distance=None, indent:int=0):
         indent_str = self._indent * indent
 
         if distance:
@@ -106,20 +107,20 @@ class Scenic3:
             try: self.code.append(f"{indent_str}{obj1} {keyword} {obj2}")
             except: self.code.append(f"{indent_str}{str(obj1)} {str(keyword)} {str(obj2)}")
 
-    def Uniform(self, seq:any, indent=0):
+    def Uniform(self, seq:Any, indent:int=0):
         indent_str = self._indent * indent
         return f"{indent_str}Uniform({seq})"
 
-    def Range(self, start:any, end:any, indent=0):
+    def Range(self, start:Any, end:Any, indent:int=0):
         indent_str = self._indent * indent
         return f"{indent_str}Range({start}, {end})"
 
-    def require(self, condition:any, indent=0):
+    def require(self, condition:Any, indent:int=0):
         indent_str = self._indent * indent
         try: self.code.append(f"{indent_str}require {condition}")
         except: self.code.append(f"{indent_str}require {str(condition)}")
 
-    def terminate(self, condition:any, indent=0):
+    def terminate(self, condition:Any, indent:int=0):
         indent_str = self._indent * indent
         try: self.code.append(f"{indent_str}terminate when {condition}")
         except: self.code.append(f"{indent_str}terminate when {str(condition)}")
